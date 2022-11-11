e0fede8c0f99028ca736ef8fbf2459e86c030354
Adi Khoiron Hasan <adieron97@gmail.com>
2022-11-11 09:52:28 +0700

    fix readme and gitignore

--- pkg/database/migrations/20221109140016_create_table_articles.up.sql
+++ pkg/database/migrations/20221109140016_create_table_articles.up.sql
@@ -1,12 +1,4 @@
--- CREATE TABLE articles 
--- (
---   id int NOT NULL AUTO_INCREMENT,
---   author varchar(60) NOT NULL,
---   title varchar(150) NOT NULL,
---   body text NOT NULL,
---   created timestamp NOT NULL,
---   PRIMARY KEY (id)
--- ) ENGINE = InnoDB;
+
 CREATE TABLE articles
 (
   id BIGSERIAL,
